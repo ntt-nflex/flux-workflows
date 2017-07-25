559ec1c21fa191a8eb04439256e930bdd4ba3f6b
Georgi Dimitrov <georgi.dimitrov@headforwards.com>
2017-07-25 16:02:35 +0100

    Bump the package version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as f:
 
 setup(
     name="flux-workflows",
-    version="0.0.3",
+    version="0.0.4",
     description="Command line tool for interacting with Flux Workflows",
     long_description=long_description,
     url='http://www.ntt.com',
